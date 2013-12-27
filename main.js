42989fdeb2d7436fd41515ad6cdc2fb4bab28f21
Dileep Singh <dileep2008apex@gmail.com>
2013-12-26 22:06:31 -0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,16 @@
+var red = [374, 100, 63];
+var orange = [40, 100, 60];
+var green = [75, 100, 40];
+var blue = [196, 77, 55];
+var purple = [82, 50, 60];
+
+var myName = "Dileep Singh";
+letterColors = [red, orange, green];
+if (10>3){
+    bubbleShape="circle";
+}
+else {
+    bubbleShape="square";
+}
+drawName (myName, letterColors);
+bounceBubbles();    
